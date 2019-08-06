656039aea5d85ebda4a4b8e0c8b789cb88a2f9b6
Carlos Villalobos <carliyipxd@gmail.com>
2019-08-06 11:51:25 -0500

    [+] Add react-router-dom and font-awesome packages.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <FontAwesomeIcon icon={faCoffee} />
         <a
           className="App-link"
           href="https://reactjs.org"
