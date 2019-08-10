cad43f264fdb9b2bc9b2b664e80a7dc445d7fa9e
Carlos Villalobos <carliyipxd@gmail.com>
2019-08-10 10:24:12 -0500

    [+] Creating the view Acumular

--- src/Views/Screens/Acumular.js
+++ src/Views/Screens/Acumular.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import '../../Styles/Views/Acumular.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQrcode } from '@fortawesome/free-solid-svg-icons'
 
 
 const Acumular = () =>{
     return(
         <div className="container-master">
-            <div className="container-read-qr">
-                <p className="text-universal-categories">Promociones</p>
-            </div>
+           <div className="container-master-acumular">
+            <div className="container-master-text-qr">
+                    <div className="container-text-qr">
+                        <p><FontAwesomeIcon icon={faQrcode} /> Escanear codigo QR</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="container-qr-read"></div>
+                </div>
+           </div>
         </div>
     )
 }
